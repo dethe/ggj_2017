a3f5715018b34f0e4896da7325fdb02114ee0fe4
dethe <dethe@livingcode.org>
2017-01-22 15:33:41 -0800

    winnable, but not playable

---
+++ js/win.js
@@ -0,0 +1,29 @@
+'use strict';
+(function(){
+var text_x = 50;
+var header_font = {font: '18pt Helvetica', fill: '#FFF'}
+var body_font = {font: '14px Helvetica', fill: '#FFF'}
+
+var header_text = 'You Win!';
+var body_text = [
+    'Congratulations! You have escaped the teapot!'
+];
+
+var Tempest = Tempest || {};
+Tempest.Win = function(){};
+Tempest.Win.prototype = {
+    create: function(){
+        console.log('Created Win');
+        var game = this;
+        game.add.text(text_x, 50, header_text, header_font);
+        body_text.forEach(function(text, index){
+            game.add.text(text_x, 150 + 30 * index, text, body_font);
+        });
+        this.input.keyboard.enabled = true;
+        this.input.keyboard.addCallbacks({}, null, null, function(){
+            game.state.start('Intro');
+            game.input.keyboard.stop();
+        });
+    }
+};
+}();
